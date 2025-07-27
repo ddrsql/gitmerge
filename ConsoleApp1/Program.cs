f058864af8bc047d1e60566f543618ddee84db1d
ddrsql <ddrsql>
2025-07-27 21:56:32 +0800

    feat: user2-json

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,23 @@
 ﻿using System;
+using System.Text.Json;
 
 internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var str = string.Empty;
+        Test(str.ToString());
+        Console.WriteLine(str);
+    }
+
+    private static void Test(string description)
+    {
+        var str = JsonSerializer.Serialize(new
+        {
+            Name = "Test",
+            Description = description
+        });
+        Console.WriteLine(str);
     }
 }
