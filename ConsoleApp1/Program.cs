5e94b20e190cffccf96972c24348709ce5fb4f5e
ddrsql <ddrsql>
2025-07-27 21:52:29 +0800

    feat: user1-string

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Text;
 
 internal class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        var str = StringStringBuilder();
+        Console.WriteLine(str);
+    }
+
+    private static StringBuilder StringStringBuilder()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append("User1");
+        stringBuilder.Append("StringBuilder");
+        return stringBuilder;
     }
 }
